b8758265491dbb89a53166aeeb689ccbc9786a12
Captain-0 <sufiyan.in.tech+github@gmail.com>
2022-12-01 15:42:55 +0530

    defined the main screen

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import messagebox
+import base64
+import os
+
+
+def main_screen():
+    screen = Tk()
+    screen.geometry('375x398')
+    screen.title("ENCRYPT/DECRYPT")
+    screen.mainloop()
+
+
+main_screen()
