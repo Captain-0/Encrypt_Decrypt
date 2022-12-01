a69ecd0a7c03fda576ee6143b3f457cef8ca75cd
Captain-0 <sufiyan.in.tech+github@gmail.com>
2022-12-01 17:20:13 +0530

    added encryption method

--- main.py
+++ main.py
@@ -4,7 +4,44 @@ import base64
 import os
 
 
+def encrypt():
+    password = code.get()
+
+    if password == "admin":
+        screen1 = Toplevel(screen)
+        screen1.title("ENCRYPTION")
+        screen1.geometry("400x200")
+        screen1.configure(bg='black')
+
+        message = text1.get(1.0, END)
+        encode_message = message.encode("ascii")
+        base64_bytes = base64.b64encode(encode_message)
+        encrypt = base64_bytes.decode("ascii")
+
+        Label(screen1, text="ENCRYPT", font="arial", fg="black", bg='gray').place(x=10, y=0)
+        text2 = Text(screen1, font="Robote 10", bg="white", relief=GROOVE, wrap=WORD, bd=0)
+        text2.place(x=10, y=40, width=380, height=150)
+
+        text2.insert(END, encrypt)
+
+    elif password == '':
+        messagebox.showerror("encryption", "Input Secret Key")
+
+    elif password != "admin":
+        messagebox.showerror("encryption", "Invalid Secret Key")
+
+
+
+
+def decrypt():
+    pass
+
+
 def main_screen():
+    global screen
+    global code
+    global text1
+
     screen = Tk()
     screen.geometry('380x398')
 
@@ -26,8 +63,8 @@ def main_screen():
     code = StringVar()
     Entry(textvariable=code, width=20, bd=0, font=('arial', 25), show="*").place(x=10, y=200)
 
-    Button(text="ENCRYPT", height='2', width=23, bg='white', fg='black', bd=0).place(x=10, y=253)
-    Button(text="DECRYPT", height='2', width=23, bg="black", fg='white', bd=0).place(x=200, y=253)
+    Button(text="ENCRYPT", height='2', width=23, bg='white', fg='black', bd=0, command=encrypt).place(x=10, y=253)
+    Button(text="DECRYPT", height='2', width=23, bg="black", fg='white', bd=0, command=decrypt).place(x=200, y=253)
     Button(text="RESET", height='2', width=50, bg='grey', fg='black', bd=1, command=reset).place(x=10, y=300)
 
     screen.mainloop()
