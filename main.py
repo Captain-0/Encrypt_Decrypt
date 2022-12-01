0e4a6fab194bc5a326d6aded907ea9f1930afffd
Captain-0 <sufiyan.in.tech+github@gmail.com>
2022-12-01 15:49:33 +0530

    added icon

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import os
 def main_screen():
     screen = Tk()
     screen.geometry('375x398')
+
+    image_icon = PhotoImage(file="image/lock_and_key.png")
+    screen.iconphoto(False, image_icon)
+
     screen.title("ENCRYPT/DECRYPT")
     screen.mainloop()
 
